4c60099ef5933aaf3dda9397e303de581714d5d7
yisroel <yisroelshulman16@gmail.com>
2024-11-22 11:17:50 -0500

    added the movement to the player

--- main.py
+++ main.py
@@ -20,8 +20,9 @@ def  main():
                 return
         screen.fill((0,0,0))
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
-        dt = dt  + (clock.tick(60) / 1000)
+        dt += (clock.tick(60) / 1000)
 
 if __name__ == "__main__":
     main()
